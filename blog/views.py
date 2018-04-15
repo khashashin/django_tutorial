59993c577f6954164d41badc602cd19f395b9db0
khashashin <khashashin@gmail.com>
2018-04-15 23:00:40 +0200

    modified blog urls and view

--- blog/views.py
+++ blog/views.py
@@ -6,9 +6,9 @@ from .models import Post
 from .forms import PostForm
 
 
-def post_list(request):
+def index(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/index.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
