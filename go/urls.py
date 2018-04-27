5219b521ebeb0767f0fa62e0dcda938e4a93a588
khashashin <khashashin@gmail.com>
2018-04-27 20:00:21 +0200

    added scrumboard urls to project urls

--- go/urls.py
+++ go/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('catalog/', include('catalog.urls')),
     path('test/', RedirectView.as_view(url='/catalog/', permanent=True)),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('scrumboard/', include('scrumboard.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
