194a450b482c78072526d47f56595d150264c43e
khashashin <khashashin@gmail.com>
2018-05-01 23:11:03 +0200

    temp function to do

---
+++ go/static/home/js/home.js
@@ -0,0 +1,10 @@
+// (function(){
+//   'use strict';
+//
+//   angular.module('api_login', []).controller('ApiLoginController', ['$scope', '$http', ApiLoginController]);
+//   function ApiLoginController($scope, $http) {
+//     $scope.login = function() {
+//       $http.get('/scrumboard/lists/')
+//     }
+//   }
+// }());
