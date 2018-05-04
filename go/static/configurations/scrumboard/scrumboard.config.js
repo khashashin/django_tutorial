1c1434d561d4be0065fafb0ce484261847da7081
khashashin <khashashin@gmail.com>
2018-05-04 22:42:38 +0200

    moved templates to angular template manager

--- go/static/configurations/scrumboard/scrumboard.config.js
+++ go/static/configurations/scrumboard/scrumboard.config.js
@@ -1,6 +1,18 @@
 (function() {
   'use strict';
-  angular.module('scrumboard.demo').run(['$http', start_csrf]);
+  angular.module('scrumboard.demo').config(['$routeProvider', config]).run(['$http', start_csrf]);
+
+  function config($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: '/static/angular_templates/scrumboard/scrumboard.html',
+        controller: 'ScrumboardController',
+      })
+      .when('/login', {
+        templateUrl: '/static/angular_templates/scrumboard/login.html'
+      })
+      .otherwise('/');
+  }
 
   function start_csrf ($http) {
     $http.defaults.xsrfHeaderName = 'X-CSRFToken';
--- go/static/scrumboard/js/scrumboard.js
+++ go/static/scrumboard/js/scrumboard.js
@@ -1,7 +1,7 @@
 (function(){
   'use strict';
 
-  angular.module('scrumboard.demo', []).controller('ScrumboardController', ['$scope', '$http', ScrumboardController]);
+  angular.module('scrumboard.demo', ['ngRoute']).controller('ScrumboardController', ['$scope', '$http', ScrumboardController]);
   function ScrumboardController($scope, $http) {
     $scope.add = function(list, title) {
       var card = {
